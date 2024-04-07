cfc3e345ab45a44b093945d53b8766194735ebc5
Camila <alvescamila87@gmail.com>
2024-04-07 18:01:07 -0300

    class

---
+++ poo/src/classe/Carro.java
@@ -0,0 +1,10 @@
+package classe;
+
+public class Carro {
+	
+	String fabricante;
+	String modelo;
+	String cor;
+	int anoDeFabricacao;
+
+}
---
+++ poo/src/classe/Principal.java
@@ -0,0 +1,14 @@
+package classe;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Carro meuCarro; //declaramos a variável
+		meuCarro = new Carro(); //instanciamos a variável com um objeto do tipo Carro
+		
+		Carro seuCarro = new Carro(); // objetos diferentes
+		
+	}
+
+}
