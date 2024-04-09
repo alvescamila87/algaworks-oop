af3c5529a05ed296854608a31adaa37f6668db12
Camila <alvescamila87@gmail.com>
2024-04-09 17:51:04 -0300

    accessing instance variables from class

--- poo/src/classe/Principal.java
+++ poo/src/classe/Principal.java
@@ -9,6 +9,35 @@ public class Principal {
 		
 		Carro seuCarro = new Carro(); // objetos diferentes
 		
+		meuCarro.fabricante = "VW";
+		meuCarro.modelo = "Polo";
+		meuCarro.anoDeFabricacao = 2024;
+		meuCarro.cor = "Preto";
+		
+		seuCarro.fabricante = "BWM";
+		seuCarro.modelo = "X6";
+		seuCarro.anoDeFabricacao = 2023;
+		seuCarro.cor = "Branco";
+		
+		System.out.println("Meu carro");
+		System.out.println("---------------------");
+		System.out.println("Fabricante: " + meuCarro.fabricante);
+		System.out.println("Modelo: " + meuCarro.modelo);
+		System.out.println("Ano de fabricação: " + meuCarro.anoDeFabricacao);
+		System.out.println("Cor: " + meuCarro.cor);
+		
+			
+		System.out.println("\nSeu carro");
+		System.out.println("---------------------");
+		System.out.println("Fabricante: " + seuCarro.fabricante);
+		System.out.println("Modelo: " + seuCarro.modelo);
+		System.out.println("Ano de fabricação: " + seuCarro.anoDeFabricacao);
+		System.out.println("Cor: " + seuCarro.cor);
+		
+		
+		
+		
+		
 	}
 
 }
