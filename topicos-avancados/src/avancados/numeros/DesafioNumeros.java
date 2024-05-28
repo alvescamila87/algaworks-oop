793ab84b056f4ea0ba24bcd67f547fa0ba30d71b
Camila <alvescamila87@gmail.com>
2024-05-28 17:44:14 -0300

    big decimal and decimal format challenge

---
+++ topicos-avancados/src/avancados/numeros/DesafioNumeros.java
@@ -0,0 +1,25 @@
+package avancados.numeros;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class DesafioNumeros {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Informe um valor de produto: ");
+		double valorProduto = entrada.nextDouble();
+		
+		BigDecimal bd = new BigDecimal(valorProduto);		
+		bd = bd.multiply(new BigDecimal(0.10));
+		
+		DecimalFormat formatador = new DecimalFormat("R$ #,##00.00");
+		String valorMonetario = formatador.format(bd);
+		
+		System.out.println("Somente os 10% do valor do produto: " + valorMonetario + ".");
+	}
+
+}
